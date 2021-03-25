f7b8d225af379b87b8360772dd42936ff6c70016
Lunush <76045303+lunush@users.noreply.github.com>
2021-03-25 08:46:48 +0300

    Setup GraphQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,25 @@
 import { MikroORM } from "@mikro-orm/core";
 import config from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(config);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000);
 };
 
 main().catch((error) => console.log(error));
