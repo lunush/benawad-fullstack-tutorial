3167d673606e602a238e101cfa9caa5d31b5aefb
Lunush <76045303+lunush@users.noreply.github.com>
2021-03-28 17:02:02 +0300

    Add createPost route

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Arg, Ctx, Query, Resolver, Int } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -10,10 +10,18 @@ export class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id", () => Int) id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const newPost = em.create(Post, { title });
+    await em.persistAndFlush(newPost);
+
+    return newPost;
   }
 }
