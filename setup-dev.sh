4682ff5f2889b74f1a182d0da08390e47f7c1a63
Lunush <76045303+lunush@users.noreply.github.com>
2021-04-10 15:27:19 +0300

    Prevent postgres container error

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,2 +1,4 @@
-podman run --rm -d -p 6379:6379 --name lireddit-reddis docker.io/redis:alpine
+#!/bin/sh
+mkdir -p postgres
+podman run --rm -d -p 6379:6379 --name lireddit-redis docker.io/redis:alpine
 podman run --rm -d -p 5432:5432 --name lireddit-postgres -e POSTGRES_DB=lireddit -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres docker.io/postgres:alpine
