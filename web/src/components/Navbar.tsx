94dd3952b047302dc372d724b530fe7e67dbd197
Lunush <76045303+lunush@users.noreply.github.com>
2021-05-11 09:39:11 +0300

    Update NavBar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -12,34 +12,36 @@ const Navbar: React.FC<Props> = () => {
   });
 
   return (
-    <Flex p={4} bg="teal" color="white" w="100%" alignItems="center">
-      <NextLink href="/">
-        <Link mr="auto">
-          <Heading fontSize="1.5rem">LiReddit</Heading>
-        </Link>
-      </NextLink>
-      {!data?.me ? (
-        <>
-          <NextLink href="/login">
-            <Link mr="1.5rem">Login</Link>
-          </NextLink>
-          <NextLink href="/register">
-            <Link>Register</Link>
-          </NextLink>
-        </>
-      ) : (
-        <>
-          <Box mr="1.5rem">{data.me.username}</Box>
-          <Button
-            onClick={() => logout()}
-            isLoading={logoutFetching}
-            variant="link"
-            color="white"
-          >
-            Logout
-          </Button>
-        </>
-      )}
+    <Flex p={4} bg="teal" color="white" w="100%">
+      <Flex alignItems="center" flex={1} maxW={800} mx="auto">
+        <NextLink href="/">
+          <Link mr="auto">
+            <Heading fontSize="1.5rem">LiReddit</Heading>
+          </Link>
+        </NextLink>
+        {!data?.me ? (
+          <>
+            <NextLink href="/login">
+              <Link mr="1.5rem">Login</Link>
+            </NextLink>
+            <NextLink href="/register">
+              <Link>Register</Link>
+            </NextLink>
+          </>
+        ) : (
+          <>
+            <Box mr="1.5rem">{data.me.username}</Box>
+            <Button
+              onClick={() => logout()}
+              isLoading={logoutFetching}
+              variant="link"
+              color="white"
+            >
+              Logout
+            </Button>
+          </>
+        )}
+      </Flex>
     </Flex>
   );
 };
